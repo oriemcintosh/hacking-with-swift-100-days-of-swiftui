de83dd4029761bd514ea41b466d6e97cfafcd642
Orie McIntosh <69773054+oriemcintosh@users.noreply.github.com>
2026-06-01 15:02:01 -0400

    Add practice strings.

---
+++ Day-01/day-01-strings.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+let favouriteNBAPlayer = "Kobe Bryant"
+let championshipsWon = "🏆🏆🏆🏆🏆"
+
+let kobeInfo = """
+Kobe Bryant was a professional basketball player who played for the Los Angeles Lakers in the NBA. He won five championships during his career and is widely regarded as one of the greatest basketball players of all time.
+"""
+
+let nameLength = favouriteNBAPlayer.count
+print(nameLength)
+
+let goat = "g.o.a.t."
+
+print(goat.uppercased())
+
+let firstNameCheck = favouriteNBAPlayer.hasPrefix("Kobe")
+print(firstNameCheck)
+
+let lastNameCheck = favouriteNBAPlayer.hasSuffix("Bryant")
+print(lastNameCheck)
+
