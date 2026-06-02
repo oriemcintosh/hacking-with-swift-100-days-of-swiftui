daaffc92141ae7126345ac76716c5c907b50dd2d
Orie McIntosh <69773054+oriemcintosh@users.noreply.github.com>
2026-06-01 23:55:44 -0400

    Add decimal number exercise.

---
+++ Day-01/day-01-store-decimal-numbers.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+let decimalNumber = 0.4
+var doubleDecimalNumber = decimalNumber * 2
+
+let decimalNumber2 = 0.1
+let integerNumber = 1
+
+print(decimalNumber2 + Double(integerNumber))
