db0fa9960002e900c602b1d76833e3ffba53ab2d
Orie McIntosh <69773054+oriemcintosh@users.noreply.github.com>
2026-06-01 23:39:06 -0400

    Add whole numbers exercise.

--- Day-01/day-01-store-whole-numbers.playground/Contents.swift
+++ Day-01/day-01-store-whole-numbers.playground/Contents.swift
@@ -1,3 +1,10 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let theBigPayBack = 40_000
+let theBigPayBackAnnual = theBigPayBack * 12
+
+var counter = 10
+counter += 2
+
+var multiplesOfFour = counter.isMultiple(of: 4)
+print(1200.isMultiple(of: 4))
